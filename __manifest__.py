6724b81c5c8b7d3a89942002a7421b9b220eeaea
Fahad Khan <fkhan@hesabe.com>
2020-10-19 16:32:58 +0300

    Fixed bugs for SSL

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hesabe Payment Acquirer',
+    'category': 'Payment Acquirer',
+    'summary': 'Payment Acquirer: Hesabe Implementation',
+    'description': """
+
+    Hesabe Payment Gateway.
+    """,
+    'depends': ['payment'],
+    'data': [
+        'views/payment_views.xml',
+        'views/payment_hesabe_templates.xml',
+        'data/payment_acquirer_data.xml',
+    ],
+    'post_init_hook': 'create_missing_journal_for_acquirers',
+}
